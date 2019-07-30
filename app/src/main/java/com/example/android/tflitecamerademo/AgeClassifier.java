efa2ba3ad75d1f0104a280a6adc10db556e3bd6f
Mengti Sun <mengtisun@berkeley.edu>
2019-07-30 16:44:25 +0800

    first commit

---
+++ app/src/main/java/com/example/android/tflitecamerademo/AgeClassifier.java
@@ -0,0 +1,27 @@
+package com.example.android.tflitecamerademo;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.os.SystemClock;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import org.tensorflow.lite.Interpreter;
+
+public class AgeClassifier {
+
+
+}
